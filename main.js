56f7aa218894d9501133a90b032e179ecaae2904
Clara <clara.marchal@edu.devinci.fr>
2025-01-14 09:41:02 +0100

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("Hello, world!"); // Exemple d'affichage dans la console 
+
+signe = window.alert("Cette partie est un Quizz, vous devez trouver les bons titres pour les bons albums selon leur pochette");
+
+var a = 1
+var b = 2
+
+function oeration(a,b){
+   console.log( a==b)
+}
