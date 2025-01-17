34232e9d60e3b0e9b3f12f7c09794116dfbf59b7
Clara <clara.marchal@edu.devinci.fr>
2025-01-17 11:34:20 +0100

    update

---
+++ formulaire.js
@@ -0,0 +1,94 @@
+let form = document.querySelector('#form');
+let errorContainer = document.getElementById('errorList');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche l'envoi du formulaire
+    errorContainer.innerHTML = '';
+    
+
+    let mail = document.getElementById('mail');
+    let mailPattern = "^ (?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*., ?]).+$" // Expression régulière pour valider l'email
+
+    if (mail.value.trim() === '') { // verifie que le mail est vide, trim supprime les espaces blancs
+        console.log('Mail invalide : champ vide.');
+    } else if (!mailPattern.test(mail.value.trim())) { // vérifie si le mail n'est pas égal au pattern
+        mail.classList.add('invalide'); 
+        mail.classList.remove('sucess');
+     
+    } else {
+        console.log('Mail Valide')
+        mail.classList.add('success');// applique la class css success
+        mail.classList.remove('invalide');
+    }
+
+
+    let pseudo = document.querySelector('#pseudo');
+    let pseudoError = document.querySelector('#pseudoError')
+
+    if (pseudo.value.length >= 6) { // Vérifie si le pseudo a **au moins** 6 caractères
+        console.log('Pseudo Valide');
+        errorContainer.classList.remove('visible');
+        pseudo.classList.remove('Invalide');
+        pseudo.classList.add('success');
+
+    } else {
+        errorContainer.classList.add('visible');
+        pseudo.classList.add('invalide');
+        pseudo.classList.remove('success');
+
+        let err = document.createElement('li');
+        err.innerText = 'Le champ pseudo doit contenir au moins 6 caractères.';
+        errorContainer.appendChild(err);// Ajoute err dans le container pour être afficher
+    }
+
+      // Validation du mot de passe
+      let password = document.querySelector('#password');
+      let passCheck = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*., ?]).{10,}$");
+
+    // Vérification du mot de passe
+    if (password.value.length < 10 || !passCheck.test(password.value)) {
+        password.classList.add('invalide');
+        errorContainer.classList.add('visible'); 
+        errorContainer.classList.remove('error'); 
+        password.classList.remove('success');
+        
+        
+
+        let err = document.createElement('li');//Créer une liste
+        err.innerText = 'Le mot de passe doit contenir au moins 10 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.';
+        errorContainer.appendChild(err);// Ajoute err dans le container pour être afficher 
+    } else {
+        console.log('Mot de passe Valide'); 
+        password.classList.add('success');// applique la classe success qui rends le fond vert
+        password.classList.remove('invalide');
+    }
+
+
+});
+
+
+
+let ListMenu = document.querySelectorAll('li');
+
+
+ListMenu.forEach(item =>{
+    item.addEventListener('click',function(){
+        this.classList.toggle('active');
+
+        if (this.classList.contains('tableau1')) {
+            document.getElementById('contenu1').classList.remove('hide')
+            document.getElementById('contenu1').classList.add('afficher-contenu')
+        }
+    })
+})
+
+//    let successContainer = document.querySelector('.message-success')
+//   successContainer.classList.remove('visible')
+
+//   if(pseudo.classList.contains('success')&&
+//      mail.classList.contains('success')&&
+//      password.classList.contains('success')&&
+//      passwordRepeat.classList.contains('success')
+//   ){
+//  successContainer.classList.add('visible')
+//    }
